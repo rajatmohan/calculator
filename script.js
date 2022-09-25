805956bdea033f3b348d5ab9730b05767340b399
rajatmohan <rajatm2196@gmail.com>
2022-09-25 20:06:07 +0530

    Create function to opearte expression

--- script.js
+++ script.js
@@ -14,3 +14,23 @@ const divide = (a, b) => {
     return a/b;
 };
 
+const operate = (operator, a, b) => {
+    a = Number(a)
+    b = Number(b)
+    if(isNaN(a) || isNaN(b)) {
+        return null;
+    }
+    switch(operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        default:
+            return null;
+    }
+}
+
