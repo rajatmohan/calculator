5cd9f404c4ae2e9114f0a21c3b934ce74e17bb55
rajatmohan <rajatm2196@gmail.com>
2022-09-27 23:19:56 +0530

    Populate display on numeric buttons click

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+let screenValue = "";
+
+const calculatorButtonContainer = document.querySelector(".calculator-buttons");
+const calculatorButtons = calculatorButtonContainer.querySelectorAll("button");
+
+const calculatorScreenElement = document.querySelector(".calculator-screen")
+
+console.log(calculatorScreenElement)
+
+
 const add = (a, b) => {
     return a+b;
 };
@@ -33,4 +43,44 @@ const operate = (operator, a, b) => {
             return null;
     }
 }
+
+const numericButtonClick = (value)=> {
+    screenValue = screenValue + value;
+    calculatorScreenElement.textContent = screenValue;
+}
+
+const buttonClick = (event)=> {
+    buttonTextContent = event.target.textContent;
+    console.log(buttonTextContent)
+    if(['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'].includes(buttonTextContent)) {
+        numericButtonClick(Number(buttonTextContent));
+    }
+    else if(['/', '+', '-', 'x', '%'].includes(buttonTextContent)) {
+        console.log("op");
+    }
+    else if(buttonTextContent === '=') {
+        console.log("eq");
+    }
+    else if(buttonTextContent === 'C') {
+        console.log("c");
+    }
+    else if(buttonTextContent === 'AC') {
+        console.log("ac");
+    }
+    else if(buttonTextContent === '+/-') {
+        console.log("+/-");
+    }
+    else if(buttonTextContent === '.') {
+        console.log(".");
+    }
+    else {
+        console.log("err");
+    }
+}
+
+calculatorButtons.forEach(button => {
+    button.addEventListener("click", buttonClick);
+});
+
+
 
